a2a74aeef3c8014d3ee836cfc25c64d9ff02fefe
Everaldo Cardoso De Araújo <everaldocardoso.github@gmail.com>
2019-03-19 22:18:53 -0300

    Adicionando fragments e adapters

---
+++ app/src/main/java/br/com/newoutsourcing/walletofclients/Views/Activitys/RegisterClientActivity.java
@@ -0,0 +1,28 @@
+package br.com.newoutsourcing.walletofclients.Views.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import br.com.newoutsourcing.walletofclients.R;
+import br.com.newoutsourcing.walletofclients.Views.Adapters.SectionsPagerAdapter;
+
+public class RegisterClientActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_client);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+}
---
+++ app/src/main/java/br/com/newoutsourcing/walletofclients/Views/Fragments/LegalPersonFragment.java
@@ -0,0 +1,35 @@
+package br.com.newoutsourcing.walletofclients.Views.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.newoutsourcing.walletofclients.R;
+
+public class LegalPersonFragment extends Fragment {
+
+    public LegalPersonFragment() {
+    }
+
+    public static LegalPersonFragment newInstance() {
+        LegalPersonFragment fragment = new LegalPersonFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_legal_person, container, false);
+    }
+}
