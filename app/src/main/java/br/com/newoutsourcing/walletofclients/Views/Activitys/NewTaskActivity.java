401de22daef78ee2cc7f74ed8a2a7317044f5de0
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2019-08-08 22:40:36 -0300

    Trabalhando na tarefa de salvar as tasks.

--- app/src/main/java/br/com/newoutsourcing/walletofclients/Views/Activitys/NewTaskActivity.java
+++ app/src/main/java/br/com/newoutsourcing/walletofclients/Views/Activitys/NewTaskActivity.java
@@ -7,20 +7,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TimePicker;
 import java.util.Calendar;
 import br.com.jansenfelipe.androidmask.MaskEditTextChangedListener;
+import br.com.newoutsourcing.walletofclients.Objects.Tasks;
 import br.com.newoutsourcing.walletofclients.Tools.FunctionsTools;
 import br.com.newoutsourcing.walletofclients.R;
 import br.com.newoutsourcing.walletofclients.Views.Bases.BaseActivity;
 import butterknife.BindView;
 
+import static br.com.newoutsourcing.walletofclients.Repository.Database.Configurations.SessionDatabase.TB_TASKS;
+
 public class NewTaskActivity extends BaseActivity {
 
+    protected @BindView(R.id.idEdtTaksTitle) EditText idEdtTaksTitle;
+    protected @BindView(R.id.idSpnTaskClient) Spinner idSpnTaskClient;
     protected @BindView(R.id.idEdtTaksDate) EditText idEdtTaksDate;
     protected @BindView(R.id.idEdtTaksHour) EditText idEdtTaksHour;
     protected @BindView(R.id.idSwtTaskDiaInteiro) Switch idSwtTaskDiaInteiro;
+    protected @BindView(R.id.idEdtClientPFObservation) EditText idEdtClientPFObservation;
     protected @BindView(R.id.idBtnSave) Button idBtnSave;
     protected @BindView(R.id.idBtnClose) Button idBtnClose;
 
@@ -43,7 +50,48 @@ public class NewTaskActivity extends BaseActivity {
 
     private View.OnClickListener onClickClose = view -> FunctionsTools.closeActivity(NewTaskActivity.this);
 
-    private View.OnClickListener onClickSave = view -> FunctionsTools.showSnackBarLong(view,"Em desenvolvimento, aguarde...");
+    private View.OnClickListener onClickSave = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            try{
+
+                if (idEdtTaksTitle.getText().toString().isEmpty()){
+
+                }
+
+                if (idSpnTaskClient.getSelectedItemId() <= 0){
+
+                }
+
+                if (idEdtTaksDate.getText().toString().isEmpty()){
+
+                }
+
+                if (idEdtTaksHour.getText().toString().isEmpty()){
+
+                }
+
+                if (idEdtClientPFObservation.getText().toString().isEmpty()){
+
+                }
+
+                Tasks tasks = new Tasks();
+                tasks.setTitle(idEdtTaksTitle.getText().toString());
+                tasks.setClienteId(idSpnTaskClient.getSelectedItemId());
+                tasks.setAllDay(idSwtTaskDiaInteiro.isChecked()? 1 : 0);
+                tasks.setDate(idEdtTaksDate.getText().toString());
+                tasks.setHour(idEdtTaksHour.getText().toString());
+                tasks.setObservation(idEdtClientPFObservation.getText().toString());
+
+                tasks.setTasksId(TB_TASKS.Insert(tasks));
+
+                FunctionsTools.showSnackBarLong(idView, "Tarefa salva com sucesso!");
+
+            }catch (Exception ex){
+                FunctionsTools.showSnackBarLong(idView,ex.getMessage());
+            }
+        }
+    };
 
     private View.OnClickListener onClickAllDay = new View.OnClickListener() {
         @Override
