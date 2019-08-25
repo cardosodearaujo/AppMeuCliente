1deca23893f519226830c44d167e0a1f230eb08a
Everaldo Cardoso de Araújo <everaldocardoso.github@gmail.com>
2019-08-25 14:47:56 -0300

    Mudanças de interface. Mudanças na logo do app. Mudanças no controle de versões.

--- app/src/main/java/br/com/newoutsourcing/walletofclients/Views/Activitys/ContactActivity.java
+++ app/src/main/java/br/com/newoutsourcing/walletofclients/Views/Activitys/ContactActivity.java
@@ -3,6 +3,8 @@ package br.com.newoutsourcing.walletofclients.Views.Activitys;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import br.com.newoutsourcing.walletofclients.Tools.FunctionsTools;
 import br.com.newoutsourcing.walletofclients.R;
 import br.com.newoutsourcing.walletofclients.Views.Bases.ActivityBase;
@@ -12,6 +14,8 @@ public class ContactActivity extends ActivityBase {
 
     protected @BindView(R.id.idBtnContactClose) Button idBtnContactClose;
     protected @BindView(R.id.idWebView) WebView idWebView;
+    protected @BindView(R.id.idBtnNews) ImageButton idBtnNews;
+    private boolean loadReleases = false;
 
     public ContactActivity() {
         super(R.layout.activity_contact);
@@ -20,8 +24,21 @@ public class ContactActivity extends ActivityBase {
     @Override
     protected void onConfiguration() {
         this.idBtnContactClose.setOnClickListener(this.onClickClose);
+        this.idBtnNews.setOnClickListener(this.onClickNews);
         this.idWebView.loadUrl("file:///android_asset/contact.html");
     }
 
     View.OnClickListener onClickClose = v -> FunctionsTools.closeActivity(ContactActivity.this);
+
+    View.OnClickListener onClickNews = v -> {
+        if (!this.loadReleases){
+            this.loadReleases = true;
+            this.idBtnNews.setBackgroundResource(R.mipmap.ic_arrow_red);
+            this.idWebView.loadUrl("file:///android_asset/news_releases.html");
+        }else{
+            this.loadReleases = false;
+            this.idBtnNews.setBackgroundResource(R.mipmap.ic_new_realeses);
+            this.idWebView.loadUrl("file:///android_asset/contact.html");
+        }
+    };
 }
